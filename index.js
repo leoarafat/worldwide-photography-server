28250551351fbe122aef570c9cd3184e5504c0b6
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-11-08 09:59:50 +0600

    checking get server

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`)
+})
