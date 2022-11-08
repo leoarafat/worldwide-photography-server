5145f83dcb04b876fa615611da1447b6c4604aec
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-11-08 12:40:31 +0600

    creat id api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000
@@ -31,6 +31,12 @@ async function run(){
             const query = {}
             const cursor = serviceCollection.find(query)
             const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/allServices/:id', async(req, res)=>{
+            const id = req.params.id 
+            const query = {_id: ObjectId(id)}
+            const result = await serviceCollection.findOne(query)
             res.send(result)
         })
 
