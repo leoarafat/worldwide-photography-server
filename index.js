6148cdb02123ac4cd06bf77c7c7a5521562b5a65
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-11-08 10:08:31 +0600

    implement run function

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tjc9clz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+
+    try{
+        const serviceCollection = client.db('photography').collection('photoService')
+
+
+    }
+    catch(error){
+        console.log(error.name, error.message)
+        
+    }
+
+}
+run().catch(error =>{
+    console.log(error)
+    
+})
 
 
 
